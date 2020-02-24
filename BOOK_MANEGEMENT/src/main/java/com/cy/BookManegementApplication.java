b8dd5a0546f0adccdbec6c393ea6f7cbbee43ce1
Administrator <Administrator@PC-201910101831>
2020-02-24 20:03:31 +0800

    你好

---
+++ BOOK_MANEGEMENT/src/main/java/com/cy/BookManegementApplication.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManegementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManegementApplication.class, args);
+	}
+
+}
---
+++ BOOK_MANEGEMENT/src/test/java/com/cy/BookManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
